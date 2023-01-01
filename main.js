8645b1747a10d6e7537df08cca6794c799433a46
Benj-hub <lamprecht193@gmail.com>
2023-01-01 21:12:55 +0100

    continuous change

--- main.js
+++ main.js
@@ -17,3 +17,138 @@ const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.n
 if (isIos() && !isInStandaloneMode()) {
     this.setState({ showInstallMessage: true });
 }
+
+
+/*
+created by Benjamin Lamprecht
+30.12.2022
+*/
+
+var friends = [];
+var menuBtn = document.getElementById('menuAdd');
+var subMenu1 = document.getElementById('subMenu1');
+var subMenu2 = document.getElementById('subMenu2');
+var btnClicked = false;
+var dialog = document.getElementById("comingSoon");
+
+var me = document.getElementById('owner');
+
+async function init() {
+    await dummyValues();
+
+
+    for (let i = 0; i < friends.length; i++) {
+        let friend = JSON.parse(friends[i]);
+        createLine(friend.name, friend.img);
+    }
+}
+
+menuBtn.addEventListener('click', event => {
+    btnClicked = !btnClicked;
+
+    if(btnClicked) {
+        menuBtn.style.transform = 'rotate(45deg)';
+        subMenu1.style.visibility = 'visible';
+        subMenu2.style.visibility = 'visible';
+    }
+    else {
+        menuBtn.style.transform = 'rotate(0deg)';
+        subMenu1.style.visibility = 'collapse';
+        subMenu2.style.visibility = 'collapse';
+    }
+});
+
+subMenu1.addEventListener('click', event => {
+    openWindow();
+});
+
+subMenu2.addEventListener('click', event => {
+    openWindow();
+});
+
+async function openWindow()
+{
+   dialog.showModal();
+}
+
+async function closeWindow() {
+    dialog.close();
+}
+
+init();
+
+function createFriend(name, img, attributes, messages) {
+    let friend = {};
+    friend.name = name;
+    friend.img = img;
+    friend.attributes = attributes;
+    friend.messages = messages;
+    return JSON.stringify(friend);
+}
+
+function createAttribute(title, content) {
+    let attribute = {};
+    attribute.title = title + ":";
+    attribute.content = content;
+    return JSON.stringify(attribute);
+}
+
+function createMessage(text, img) {
+    let message = {};
+    message.text = text;
+    message.img = img;
+    return JSON.stringify(message);
+}
+
+async function createLine(name, img) {
+    let section = document.getElementById("list");
+    let friend = document.createElement("div");
+    let image = document.createElement("img");
+    let nameSpan = document.createElement("span");
+
+    friend.className = "friend"
+    image.src = await img;
+    nameSpan.innerHTML = name;
+
+
+    section.appendChild(friend);
+    friend.appendChild(image);
+    friend.appendChild(nameSpan);
+
+    friend.addEventListener('click', event => {
+        friend.href = "friend.html";
+        parent.location='friend.html';
+    });
+}
+
+
+async function dummyValues() {
+    let attributeArray = [];
+
+    attributeArray.push(await createAttribute("Birthday", "31.12.1993"));
+    attributeArray.push(await createAttribute("Lieblingsfarbe", "Blau"));
+    attributeArray.push(await createAttribute("mein Haustier", "Filomena"));
+    attributeArray.push(await createAttribute("wir kennen uns seid", "05.07.2010"));
+
+    let attributes = await JSON.stringify(attributeArray);
+
+    let messageArray = [];
+    messageArray.push(await createMessage("I glaub du hast den Kontest für bestes Foto gewonnen. :)", "img\img_dummy.png"));
+
+    let messages = JSON.stringify(messageArray);
+
+    let friend = await createFriend("Stella", "img/stella.jpg", attributes, messages);
+    friends.push(friend);
+    friends.push(await createFriend("ipsum", "img/img51487947.jpg"));
+    friends.push(await createFriend("dolor", "img/img159179884.jpg"));
+    friends.push(await createFriend("sit", "img/q6d8h7l8.bmp"));
+    friends.push(await createFriend("lorem2", "img/img159179884.jpg"));
+    friends.push(await createFriend("ipsum2", "img/img51487947.jpg"));
+    friends.push(await createFriend("dolor2", "img/img159179884.jpg"));
+    friends.push(await createFriend("sit2", "img/q6d8h7l8.bmp"));
+    friends.push(await createFriend("lorem3", "img/img159179884.jpg"));
+    friends.push(await createFriend("ipsum3", "img/img51487947.jpg"));
+    friends.push(await createFriend("dolor3", "img/img159179884.jpg"));
+    friends.push(await createFriend("sit3", "img/q6d8h7l8.bmp"));
+
+}
--- scriptFriend.js
+++ scriptFriend.js
@@ -1,137 +1,131 @@
 /*
 created by Benjamin Lamprecht
-22.10.2022
+30.12.2022
 */
 
-var days = [];
-var glasJson = 0;
-var krugJson = 0;
+var friends = [];
 
-var submitBtn = document.getElementById('submit');
-var resetBtn = document.getElementById('reset');
+var sendBtn = document.getElementById('sendBtn');
+var resetBtn = document.getElementById('back');
+var friends = [];
+var friend;
+var attributes;
+var messages;
 
-var canvas = document.getElementById("myCanvas");
-var weight = localStorage.getItem("weight");
-if (weight == null) weight = 0;
-var context = canvas.getContext("2d");
-var waterMeter = document.getElementById('waterMeter');
+var dialog = document.getElementById("dialogSendMessage");
+var notification = document.getElementById("notification");
+
+async function init() {
+    await dummyValues();
+
+    friend = JSON.parse(friends[0]);
+    let attributes = JSON.parse(friend.attributes);
+    for (let i = 0; i < attributes.length; i++) {
+        let attribute = JSON.parse(attributes[i]);
+        await createAttributeLine(attribute.title, attribute.content);
+    }
+
+    let messages = JSON.parse(friend.messages);
+    for (let i = 0; i < messages.length; i++) {
+        let message = JSON.parse(messages[i]);
+        await createMessageLine(message.text, message.img);
+    }
+
+}
+
+async function openWindow()
+{
+   dialog.showModal();
+}
+
+async function sendMessage() {
+    dialog.close();
+
+    notification.innerHTML = document.getElementById('content').value;
+    notification.showModal();
+
+    setTimeout(function (){
+        notification.close();
+        console.log("goddammit!")
+    }, 3000);
+}
 
 init();
 
-function init() {
-    let totalGlas = 0;
-    let totalKrug = 0;
-    if (localStorage.getItem("drinks") != null) days = JSON.parse(localStorage.getItem("drinks"));
-    for (let i = 0; i < days.length; i++) {
-        let day = JSON.parse(days[i]);
-        document.getElementById("#" + i + "Glas").innerHTML = day.glas;
-        document.getElementById("#" + i + "Krug").innerHTML = day.krug;
-        totalGlas += parseInt(day.glas);
-        totalKrug += parseInt(day.krug);
-    }
-    document.getElementById("sumOfGlas").innerHTML = totalGlas;
-    document.getElementById("sumOfKrug").innerHTML = totalKrug;
+function createFriend(name, img, attributes, messages) {
+    let friend = {};
+    friend.name = name;
+    friend.img = img;
+    friend.attributes = attributes;
+    friend.messages = messages;
+    return JSON.stringify(friend);
 }
 
-
-if (weight == 0) {
-    weight = prompt("Bitte Gewicht angeben (in kg): ");
-    document.getElementById('weight').value = weight;
-    if (weight > 75) {
-        waterMeter.optimum = 4000;
-        waterMeter.max = 4000;
-    }
+function createAttribute(title, content) {
+    let attribute = {};
+    attribute.title = title + ":";
+    attribute.content = content;
+    return JSON.stringify(attribute);
 }
 
-function fetchValues() {
-    var num = document.getElementById('quantity').value;
-    var glas = document.querySelector('input[name="amount"]:checked').value;
-
-    if (glas == 300) glasJson = num;
-    else krugJson = num;
-
-    waterMeter.value = waterMeter.value + (num * glas);
-    waterMeter.value = waterMeter.value > waterMeter.max ? waterMeter.max : waterMeter.value;
-    document.getElementById('waterMeter').value = waterMeter.value;
-
+function createMessage(text, img) {
+    let message = {};
+    message.text = text;
+    message.img = img;
+    return JSON.stringify(message);
 }
 
-setInterval(drawWater, 15);
+async function createAttributeLine(titleTxt, contentTxt) {
+    let section = document.getElementById("listAttributes");
+    let attribute = document.createElement("div");
+    let title = document.createElement("span");
+    let content = document.createElement("span");
+    let image = document.createElement("img");
+    image.src = "img/attribute.png";
 
-submitBtn.addEventListener('click', async event => {
-    event.preventDefault();
-    fetchValues();
-    document.getElementById('waterMeter').value = waterMeter.value;
-    drawWater();
-    await updateDay();
+    attribute.className = "attribute";
+    title.className = "title";
+    content.className = "content";
 
-    await saveToJson();
-    await init();
-});
+    title.innerHTML = titleTxt;
+    content.innerHTML = contentTxt;
 
-resetBtn.addEventListener('click', async event => {
-    event.preventDefault();
-    document.getElementById('waterMeter').value = 0;
-
-    let tempDay = JSON.parse(days[0]);
-    if (tempDay.date == new Date().toISOString().split('T')[0]) {
-        days.pop();
-    }
-    document.getElementById("#0Glas").innerHTML = '-';
-    document.getElementById("#0Krug").innerHTML = '-';
-    saveToJson();
-    init();
-});
-
-var x = 0;
-function drawWater() {
-    x = x > 24 ? x = 0 : x++;
-    x++;
-    let height = waterMeter.value / (waterMeter.max / 100);
-    context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
-    context.beginPath();
-    context.fillStyle = "lightblue";
-    context.rect(0, (canvas.clientHeight - (height)), canvas.clientWidth, canvas.clientHeight);
-    for (var i = 0; i < 10; i++) {
-        context.arc(((i * 25) + x - 25), ((canvas.clientHeight - (height - 15))), 25, 0, Math.PI * 2);
-    }
-    context.fill();
+    section.appendChild(attribute);
+    attribute.appendChild(image);
+    attribute.appendChild(title);
+    attribute.appendChild(content);
 }
 
-function flush() {
-    glasJson = 0;
-    krugJson = 0;
+async function createMessageLine(messageText, img) {
+    let section = document.getElementById("listMessages");
+    let container = document.createElement("div");
+    let text = document.createElement("p");
+    let image = document.createElement("img");
+
+    container.className = "message";
+    text.innerHTML = messageText;
+    image.src = await img;
+
+    section.appendChild(container);
+    container.appendChild(text);
+    container.appendChild(image);
 }
 
+async function dummyValues() {
+    let attributeArray = [];
 
-async function updateDay() {
+    attributeArray.push(await createAttribute("Birthday", "31.12.1993"));
+    attributeArray.push(await createAttribute("Lieblingsfarbe", "Blau"));
+    attributeArray.push(await createAttribute("mein Haustier", "Filomena"));
+    attributeArray.push(await createAttribute("wir kennen uns seid", "05.07.2010"));
 
-    //if days are full
-    if (days.length >= 7) days.pop();
-    //if first execution
-    if (days.length > 0) {
-        var tempDay = await JSON.parse(days[0]);
-        if (tempDay.date == new Date().toISOString().split('T')[0]) {
-            days[0] = await createDay(parseInt(tempDay.glas) + parseInt(glasJson), parseInt(tempDay.krug) + parseInt(krugJson));
-            //days[0] = await createDay(8, 8);
-            await flush();
-            return;
-        }
-    }
-    let temp = await createDay(glasJson, krugJson);
-    days.unshift(temp);
-    await flush();
-}
+    let attributes = await JSON.stringify(attributeArray);
 
-function saveToJson() {
-    localStorage.setItem("weight", weight);
-    localStorage.setItem("drinks", JSON.stringify(days));
-}
+    let messageArray = [];
+    messageArray.push(await createMessage("I glaub du hast den Kontest für bestes Foto gewonnen. :)", "img/img_dummy.png"));
 
-function createDay(glas, krug) {
-    let day = {};
-    day.glas = glas;
-    day.krug = krug;
-    day.date = new Date().toISOString().split('T')[0];
-    return JSON.stringify(day);
+    let messages = JSON.stringify(messageArray);
+
+    let friend = await createFriend("Stella", "img/stella.jpg", attributes, messages);
+    friends.push(friend);
 }
